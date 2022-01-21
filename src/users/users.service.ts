a18e10072c74728c3084be7dacb5bccb1077d3cc
Andrei Sachuk <a.sachuk@softteco.com>
2022-01-21 19:22:51 +0300

    finished users table

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,22 +2,26 @@ import {Injectable} from '@nestjs/common';
 import {User} from "./users.model";
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/createUser.dto";
+import {RolesService} from "../roles/roles.service";
 
 @Injectable()
 export class UsersService {
 
   constructor(
-    @InjectModel(User) private userRepository: typeof User
+    @InjectModel(User) private userRepository: typeof User,
+    private roleService: RolesService
   ) {
   }
 
   async createUser(dto: CreateUserDto) {
     const user = await this.userRepository.create(dto);
+    const role = await this.roleService.getRoleByValue('user')
+    await  user.$set('roles', [role.id])
     return user;
   }
 
   async getAllUsers() {
-    const users = await this.userRepository.findAll();
+    const users = await this.userRepository.findAll({include: {all: true}});
     return users;
   }
 }
