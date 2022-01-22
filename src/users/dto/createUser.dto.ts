9343b9a19ed40398a14d67398825f28a9221f2f1
Andrei Sachuk <a.sachuk@softteco.com>
2022-01-22 11:32:50 +0300

    added Validation pipe

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,10 +1,15 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class CreateUserDto {
 
   @ApiProperty({example: 'user@gmail.com', description: 'User email'})
+  @IsString({message: 'Must be a string'})
+  @IsEmail({},{message: 'Incorrect email'})
   readonly email: string
 
   @ApiProperty({example: 'Qwr23rsdf!', description: 'User password'})
+  @IsString({message: 'Must be a string'})
+  @Length(4, 16, {message: 'Password must be longer than 4 and less than 16 characters'})
   readonly password: string
 }
